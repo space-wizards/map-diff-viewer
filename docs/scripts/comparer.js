9f1d848eef7d56cb86848a2377eb7c8499aed94b
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-03-16 20:38:24 +0100

    Add basic index.js, diff script and sample images

---
+++ docs/scripts/comparer.js
@@ -0,0 +1,53 @@
+function compare() {
+    const x = document.getElementsByClassName("img-comp-overlay");
+
+    for (let i = 0; i < x.length; i++) {
+        const image = x[i];
+
+        let clicked = false;
+
+        const height = image.offsetHeight;
+        const width = image.offsetWidth;
+
+        image.style.width = (width / 2) + "px";
+
+        const slider = document.createElement("DIV");
+        slider.setAttribute("class", "img-comp-slider");
+
+        image.parentElement.insertBefore(slider, image);
+
+        slider.style.top = (height / 2) - (slider.offsetHeight / 2) + "px";
+        slider.style.left = (width / 2) - (slider.offsetWidth / 2) + "px";
+
+        slider.addEventListener("mousedown", slideReady);
+        window.addEventListener("mouseup", () => clicked = false);
+        slider.addEventListener("touchstart", slideReady);
+        window.addEventListener("touchend", () => clicked = false);
+
+        function slideReady(event) {
+            event.preventDefault();
+
+            clicked = true;
+
+            window.addEventListener("mousemove", slideMove);
+            window.addEventListener("touchmove", slideMove);
+        }
+
+        function slideMove(event) {
+            if (!clicked) return false;
+
+            event = event || window.event;
+
+            const imageRect = image.getBoundingClientRect();
+            var pos = event.pageX - imageRect.left - window.pageXOffset;
+
+            if (pos < 0) pos = 0;
+            if (pos > width) pos = width;
+
+            image.style.width = pos + "px";
+            slider.style.left = image.offsetWidth - (slider.offsetWidth / 2) + "px";
+        }
+    }
+}
+
+compare();
